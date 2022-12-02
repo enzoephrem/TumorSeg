93a2e2a05f34a84202eeb5ce0a658f69d4eb55ab
Enzo <enzo.ephrem@etu.univ-lyon1.fr>
2022-12-02 09:27:42 +0100

    data_process is now compatible with few more extensions, postprocessing of the predicted mask + 3D Display arguments are not filepaths anymore but the data of the brain and or the seg itself

--- src/data_process.py
+++ src/data_process.py
@@ -3,14 +3,75 @@ import numpy as np
 import nibabel as nib
 from tensorflow.keras.utils import to_categorical
 import random
+import medpy.io
 import shutil
+import display
+
+
+
+"""
+All datasets should ahve the following form for each patient before process
+	dataset/
+	├─ BraTS20_001/
+	│  ├─ BraTS20_001_flair.nii
+	│  ├─ BraTS20_002_seg.nii
+	│  ├─ BraTS20_002_t1.nii
+	│  ├─ BraTS20_002_t1ce.nii
+	│  ├─ BraTS20_002_t2.nii
+	├─ BraTS20_002/
+	│  ├─ BraTS20_002_flair.nii
+	│  ├─ BraTS20_002_seg.nii
+	│  ├─ BraTS20_002_t1.nii
+	│  ├─ BraTS20_002_t1ce.nii
+	│  ├─ BraTS20_002_t2.nii
+	├─ BraTS20_XXX/
+	│  ├─ BraTS20_XXX_flair.nii
+	│  ├─ BraTS20_XXX_seg.nii
+	│  ├─ BraTS20_XXX_t1.nii
+	│  ├─ BraTS20_XXX_t1ce.nii
+	│  ├─ BraTS20_XXX_t2.nii
+	├─ BraTS20_YYY/
+	│  ├─ BraTS20_YYY_flair.nii
+	│  ├─ BraTS20_YYY_seg.nii
+	│  ├─ BraTS20_YYY_t1.nii
+	│  ├─ BraTS20_YYY_t1ce.nii
+	│  ├─ BraTS20_YYY_t2.nii
+
+This data processing is made for a U-Net with an input shape of 128x128x128
+"""
+
+def load(filepath):
+	"""
+	Load a brain IRM or npy array 
+	"""
+	
+	file_ext = filepath.split('.')[-1]
+	name = filepath.split('.')[-2].split('/')[-1]
+	
+	# Load inputs data
+	if file_ext == 'mha':
+		image_data, image_header = medpy.io.load(filepath)
+	elif file_ext == 'nii' or 'nii' + file_ext == 'niigz':
+		image_data = nib.load(filepath).get_fdata()
+	elif file_ext == 'npy':
+		image_data = np.load("filename")
+	else:
+		print("Make sure to enter a supported file extension (.mha / .nii / .nii.gz / .npy)")
+		return -1
+
+	print("File information:")
+	print("\t Input file name: {}".format(name))
+	print("\t Input file type: {}".format(file_ext))
+	print("\t Shape: {}".format(image_data.shape))
+	return image_data
+
+
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 
 
-
-def process_images(patient_dir_path, processed_path=""):
+def process_images(patient_dir_path, processed_dir_path="", file_ext='nii'):
 	"""
 	Scaling each image (values from 0 to 1)
 	Using MinMaxScaller from sklearn.preprocessing
@@ -18,33 +79,37 @@ def process_images(patient_dir_path, processed_path=""):
 
 	patient = patient_dir_path.split('/')[-1]
 
-	image_flair = nib.load(os.path.join(patient_dir_path, "{}_flair.nii".format(patient))).get_fdata()
+	image_flair = load(os.path.join(patient_dir_path, "{}_flair.{}".format(patient, file_ext))).get_fdata()
 	image_flair = scaler.fit_transform(image_flair.reshape(-1, image_flair.shape[-1])).reshape(image_flair.shape)
 
 	"""
 	# Not useful
-	test_image_t1 = nib.load(os.path.join(patient_dir_path, "{}/{}_t1.nii".format(patient, patient))).get_fdata()
+	test_image_t1 = load(os.path.join(patient_dir_path, "{}_t1.{}".format(patient, file_ext))).get_fdata()
 	test_image_t1 = scaler.fit_transform(test_image_t1.reshape(-1, test_image_t1.shape[-1])).reshape(test_image_t1.shape)
 	"""
 
-	image_t1ce = nib.load(os.path.join(patient_dir_path, "{}_t1ce.nii".format(patient))).get_fdata()
+	image_t1ce = load(os.path.join(patient_dir_path, "{}_t1ce.{}".format(patient, file_ext))).get_fdata()
 	image_t1ce = scaler.fit_transform(image_t1ce.reshape(-1, image_t1ce.shape[-1])).reshape(image_t1ce.shape)
 
-	image_t2 = nib.load(os.path.join(patient_dir_path, "{}_t2.nii".format(patient))).get_fdata()
+	image_t2 = load(os.path.join(patient_dir_path, "{}_t2.{}".format(patient, file_ext))).get_fdata()
 	image_t2 = scaler.fit_transform(image_t2.reshape(-1, image_t2.shape[-1])).reshape(image_t2.shape)
 
-	# Stack 3 most important channel into 1 numpy array to feed the U-Net
-	resulting_image = np.stack([image_flair,
-								image_t1ce,
-								image_t2], axis=3)
+	if image_flair.shape == image_t1ce.shape == image_t2.shape:
+		# Stack 3 most important channel into 1 numpy array to feed the U-Net
+		resulting_image = np.stack([image_flair,
+									image_t1ce,
+									image_t2], axis=3)
 
-	# Crop the image to 128x128x128
-	resulting_image = resulting_image[56:184, 56:184, 13:141] # Shape 128x128x128x3 (x4 if t1 included)
+		# Crop the image from 240x240x155 to 128x128x128
+		resulting_image = resulting_image[56:184, 56:184, 13:141] # Shape 128x128x128x3 (x4 if t1 included)
 
-	np.save(os.path.join(processed_path, "image", patient+".npy"), resulting_image)
+		np.save(os.path.join(processed_dir_path, "images", patient+".npy"), resulting_image)
+	else:
+		print("Channels don't have the same shape")
+		return -1
 
 
-def process_mask(patient_dir_path, processed_path=""):
+def process_mask(patient_dir_path, processed_dir_path="", file_ext='nii'):
 	""" 
 	Mask relabeling 4 -> 3 (label 3 is empty)
 	Then croping it to 128x128x128 images, taking out all the blanc usless information (less computing time + less bias for the Neural Network)
@@ -53,44 +118,47 @@ def process_mask(patient_dir_path, processed_path=""):
 
 	patient = patient_dir_path.split('/')[-1]
 
-	mask = nib.load(os.path.join(patient_dir_path, "{}_seg.nii".format(patient))).get_fdata()
+	mask = load(os.path.join(patient_dir_path, "{}_seg.{}".format(patient, file_ext))).get_fdata()
 	mask = mask.astype(np.uint8)
 
-	# Change the label 4 to 3
+	# Change the label 4 to 3 (the model was train with this logic)
 	mask[mask==4] = 3 
 	mask = mask[56:184, 56:184, 13:141] # Shape 128x128x128
 
-	mask = to_categorical(mask, num_classes=4)
-	np.save(os.path.join(processed_path, "mask", patient+".npy"), mask)
+	# from a label to 0 to 4 to get the output as probabilities of the label
+	# [1, 0, 0, 0] -> 0
+	# [0, 1, 0, 0] -> 1
+	# [0, 0, 1, 0] -> 2
+	# [0, 0, 0, 1] -> 3
+	mask = to_categorical(mask, num_classes=4) # num_classes 4 because converted all 4 to 3
+	np.save(os.path.join(processed_dir_path, "mask", patient+".npy"), mask)
 
 
-def process_patient(patient_dir_path, processed_path="", val=False):
+def process_patient(patient_dir_path, processed_dir_path="", val_or_test=False, file_ext='nii'):
 	"""
 	Processes the images and the mask of a single patient by calling above functions
 	"""
-	if val:
-		process_mask(patient_dir_path, processed_path)
-		process_images(patient_dir_path, processed_path)
+
+	# If True don't process the mask (no ground truth for validation and testing data)
+	if val_or_test:
+		process_mask(patient_dir_path, processed_dir_path, file_ext)
+		process_images(patient_dir_path, processed_dir_path, file_ext)
 		return True
 	else:
 		if random.randint(0,10) <= 8: # Get a ~80% chance of getting picked for training
-			process_mask(patient_dir_path, processed_path)
-			process_images(patient_dir_path, processed_path)
+			process_mask(patient_dir_path, processed_dir_path, file_ext)
+			process_images(patient_dir_path, processed_dir_path, file_ext)
 			return True
 		return False
 
 
-def process_dataset(dataset_path=None, processed_path="", val=False):
+def processd_dataset(dataset_folder_path, processed_dir_path="processed", val_or_test=False, file_ext='nii'):
 	"""
 	Process an entire dataset with a specific schema
 	"""
 
-	if dataset_path is None:
-		print("Please enter a dataset path")
-		return -1
-
-	# Create the folders in which fill the processed images following schema:
-	""" training_processed/
+	# Create the folders in which the processed images and masks will be stored following this schema:
+	""" processed/
 	├─ images/
 	│  ├─ BraTS20_Training_001.npy
 	│  ├─ BraTS20_Training_002.npy
@@ -102,19 +170,23 @@ def process_dataset(dataset_path=None, processed_path="", val=False):
 	│  ├─ BraTS20_Training_XXX.npy
 	│  ├─ BraTS20_Training_YYY.npy
 	"""
-	os.mkdir(processed_path)
-	os.mkdir(processed_path+"/images")
-	os.mkdir(processed_path+"/mask")
+
+	# try except possible
+	os.mkdir(processed_dir_path)
+	os.mkdir(processed_dir_path+"/images")
+	os.mkdir(processed_dir_path+"/mask")
 
 	# Processes each patient in the original dataset
-	for dirname, patients, filenames in os.walk(dataset_path):
+	for dirname, patients, filenames in os.walk(dataset_folder_path):
 		for patient in patients: 
-			if process_patient(os.path.join(dirname, patient), processed_path, val):
-				shutil.rmtree(os.path.join(dirname, patient))
+			if process_patient(os.path.join(dirname, patient), processed_dir_path, val_or_test, file_ext):
+				# uncomment the line below if you want to delete the raw data (convinient for online limitied storage)
+				#shutil.rmtree(os.path.join(dirname, patient))
+				pass
 
 
 
-def loadImageBis(image_dir, image_list):
+def loadImage(image_dir, image_list):
 	"""
 	Load images in a given directory for a given list
 	"""
@@ -131,10 +203,9 @@ def loadImageBis(image_dir, image_list):
 	return images
 
 
-
 def imageLoader(image_dir, image_list, mask_dir, mask_list, batch_size):
 	"""
-	image loader to give batches of images with a size of batch_size
+	Omage loader gives batches of images with a size of batch_size
 	"""
 
 	L = len(image_list) 
@@ -147,12 +218,34 @@ def imageLoader(image_dir, image_list, mask_dir, mask_list, batch_size):
 		while batch_start < L:
 			limit = min(batch_end, L)
 
-
-			X = loadImageBis(image_dir, image_list[batch_start:limit])
-			Y = loadImageBis(mask_dir, mask_list[batch_start:limit])
+			X = loadImage(image_dir, image_list[batch_start:limit])
+			Y = loadImage(mask_dir, mask_list[batch_start:limit])
 
 			yield(X, Y)
 
 			batch_start += batch_size
 			batch_end += batch_size
 
+
+
+def process_prediction(prediciton):
+	
+	# Get the highest predicted label
+	prediciton = np.argmax(prediciton, axis=4)[0,:,:,:]
+	# Replace the segmentation in a 244x244x128 array to match the original voxel of shape 240x240x155
+	result = np.zeros([240, 240, 155])
+	result[56:184, 56:184, 13:141] = prediciton
+	# Revert the label 3 to the original 4
+	result[result==3] = 4
+	return result
+
+
+def process_brats_file_submission(filepath, patient, ref):
+
+	# Open the saved prediction
+	img = load(filepath).get_fdata()
+	img = img.astype(np.float64)
+	# Get a nii image with the same affine and header as the ref
+	img = nib.Nifti1Image(img, ref.affine, ref.header)
+	# Save the nii.gz at the same location
+	img.to_filename(filepath)  # Save as NiBabel file
--- src/display.py
+++ src/display.py
@@ -9,114 +9,96 @@ from math import ceil
 import random
 
 
-def loadImage(filename=None):
+def display2D(brain_data, seg_data=None, brain_color_map='bone', seg_color_map='jet'):
 	"""
-	Load a brain IRM or npy array 
-	"""
-
-	if filename == None:
-		print("Make sure to enter an image filename")
-		return -1
-	
-	file_ext = filename.split('.')[-1]
-	name = filename.split('.')[-2].split('/')[-1]
-	
-	# Load inputs data
-	if file_ext == 'mha':
-		print("Input file: {} mha".format(name))
-		image_data, image_header = medpy.io.load(filename)
-	elif file_ext == 'nii' or 'nii' + file_ext == 'niigz':
-		print("Input file: {} nii".format(name))
-		image_data = nib.load(filename).get_fdata()
-	elif file_ext == 'npy':
-		print("Input file: {} numpy array".format(name))
-		image_data = np.load("filename")
-	else:
-		print("Make sure to enter a supported file extension (.mha / .nii / .nii.gz)")
-		return -1
-
-	print("File information:")
-	print("\t Shape: {}".format(image_data.shape))
-	return image_data
-
-
-def display2D(image_filename=None, seg_filename=None, image_color_map='bone', seg_color_map='jet', seg_alpha=0.5):
-    """
-    Displays random cut random layer for a given 1 sequence voxel
+	Displays random cut random layer for a given 1 sequence voxel
 	Option to give segmention (it displays the same cut same layer)
-    """
+	"""
 
-    image_data = loadImage(image_filename)
-    if len(image_data) < 0:
-        # length of loaded image < 0 -> error occurred
-        return -1
-
-    if seg_filename:
-        seg_data = loadImage(seg_filename)
-        if image_data.shape != seg_data.shape:
-            print("Image data and Segmentation data do not have the same shape {}!={}".format(image_data.shape, seg_data.shape))
-            return -1
-        if len(seg_data) < 0:
-			# length of loaded seg image < 0 -> error occurred
-            return -1
-        
-        fig, ax = plt.subplots(1, 2)
-        
-    
-    cut = random.randint(0, 2)
-
-    if cut == 0:
-        index = random.randint(0, image_data.shape[cut])
-        if seg_filename:
-            ax[0].set_title("YZ cut | layer: {}".format(index))
-            ax[1].set_title("Segmentation/Mask")
-            ax[0].imshow(image_data[index, :, :], cmap=image_color_map)
-            ax[1].imshow(seg_data[index, :, :], cmap=seg_color_map)
-        else:
-            plt.title("YZ cut | layer: {}".format(index))
-            plt.imshow(image_data[index, :, :], cmap=image_color_map)
-
-    elif cut == 1:
-        index = random.randint(0, image_data.shape[cut])
-        if seg_filename:
-            ax[0].set_title("XZ cut | layer: {}".format(index))
-            ax[1].set_title("Segmentation/Mask")
-            ax[0].imshow(image_data[:, index, :], cmap=image_color_map)
-            ax[1].imshow(seg_data[:, index, :], cmap=seg_color_map)
-        else:
-            plt.title("XZ cut | layer: {}".format(index))
-            plt.imshow(image_data[:, index, :], cmap=image_color_map)
-
-    elif cut == 2:
-        index = random.randint(0, image_data.shape[cut])
-        if seg_filename:
-            ax[0].set_title("XY cut | layer: {}".format(index))
-            ax[1].set_title("Segmentation/Mask")
-            ax[0].imshow(image_data[:, :, index], cmap=image_color_map)
-            ax[1].imshow(seg_data[:, :, index], cmap=seg_color_map)
-        else:
-            plt.title("XY cut | layer: {}".format(index))
-            plt.imshow(image_data[:, :, index], cmap=image_color_map)
-    else:
-        print("Something went wrong")
-    
-	
-
-def display3DCuts(image_filename=None, seg_filename=None, image_color_map='bone', seg_color_map='jet', seg_alpha=0.5):
-
-	# Loading image files
-
-	image_data = loadImage(image_filename)
-	if len(image_data) < 0:
-		# Return value of loadign image < 0 -> error occurred
+	if len(brain_data) < 0:
+		# Return value of loadign image < -1 -> error occurred
 		return -1
+	if brain_data.ndim != 3:
+		print("You should provide a 3 dimension array as brain data")
 
-	if seg_filename:
-		seg_data = loadImage(seg_filename)
-		if image_data.shape != seg_data.shape:
-			print("Image data and Segmentation data do not have the same shape {}!={}".format(image_data.shape, seg_data.shape))
+	if seg_data is not None:
+		if len(seg_data) < 0:
+			return -1
+		if seg_data.ndim != 3:
+			print("You should provide a 3 dimension array as seg data")
+		if brain_data.shape != seg_data.shape:
+			print("Image data and Segmentation data do not have the same shape {}!={}".format(brain_data.shape, seg_data.shape))
 			return -1
 
+	print("Type of brain data {}".format(brain_data.type))
+	print("Type of seg data {}".format(seg_data.type))
+	print("Brain and Segmentation data have shape {}".format(brain_data.shape))
+        
+	fig, ax = plt.subplots(1, 2)
+        
+    
+	cut = random.randint(0, 2)
+
+	if cut == 0:
+		index = random.randint(0, brain_data.shape[cut])
+		if seg_data:
+			ax[0].set_title("YZ cut | layer: {}".format(index))
+			ax[1].set_title("Segmentation/Mask")
+			ax[0].imshow(brain_data[index, :, :], cmap=brain_color_map)
+			ax[1].imshow(seg_data[index, :, :], cmap=seg_color_map)
+		else:
+			plt.title("YZ cut | layer: {}".format(index))
+			plt.imshow(brain_data[index, :, :], cmap=brain_color_map)
+
+	elif cut == 1:
+		index = random.randint(0, brain_data.shape[cut])
+		if seg_data:
+			ax[0].set_title("XZ cut | layer: {}".format(index))
+			ax[1].set_title("Segmentation/Mask")
+			ax[0].imshow(brain_data[:, index, :], cmap=brain_color_map)
+			ax[1].imshow(seg_data[:, index, :], cmap=seg_color_map)
+		else:
+			plt.title("XZ cut | layer: {}".format(index))
+			plt.imshow(brain_data[:, index, :], cmap=brain_color_map)
+
+	elif cut == 2:
+		index = random.randint(0, brain_data.shape[cut])
+		if seg_data:
+			ax[0].set_title("XY cut | layer: {}".format(index))
+			ax[1].set_title("Segmentation/Mask")
+			ax[0].imshow(brain_data[:, :, index], cmap=brain_color_map)
+			ax[1].imshow(seg_data[:, :, index], cmap=seg_color_map)
+		else:
+			plt.title("XY cut | layer: {}".format(index))
+			plt.imshow(brain_data[:, :, index], cmap=brain_color_map)
+    
+	
+
+def display3DCuts(brain_data, seg_data=None, brain_color_map='bone', seg_color_map='jet', seg_alpha=0.5):
+	"""
+	Display a brain voxel seen from 3 different angles Coronal, Sagittal and Horizontal
+	with sliders to go through the layers
+	"""
+
+
+	if len(brain_data) < 0:
+		# Return value of loadign image < -1 -> error occurred
+		return -1
+	if brain_data.ndim != 3:
+		print("You should provide a 3 dimension array as brain data")
+
+	if seg_data is not None:
+		if len(seg_data) < 0:
+			return -1
+		if seg_data.ndim != 3:
+			print("You should provide a 3 dimension array as seg data")
+		if brain_data.shape != seg_data.shape:
+			print("Image data and Segmentation data do not have the same shape {}!={}".format(brain_data.shape, seg_data.shape))
+			return -1
+
+	print("Type of brain data {}".format(brain_data.type))
+	print("Type of seg data {}".format(seg_data.type))
+	print("Brain and Segmentation data have shape {}".format(brain_data.shape))
 
 	# Plot with 3 Columns
 	fig, ax = plt.subplots(1, 3)
@@ -128,43 +110,43 @@ def display3DCuts(image_filename=None, seg_filename=None, image_color_map='bone'
 	axlayer_Z = plt.axes([0.25, 0.1, 0.30, 0.03])
 
 	# Get aprox the middle images for each of X Y Z
-	half_X = ceil((image_data.shape[0] - 1)/2)
-	half_Y = ceil((image_data.shape[1] - 1)/2)
-	half_Z = ceil((image_data.shape[2] - 1)/2)
+	half_X = ceil((brain_data.shape[0] - 1)/2)
+	half_Y = ceil((brain_data.shape[1] - 1)/2)
+	half_Z = ceil((brain_data.shape[2] - 1)/2)
 
 	# Create Slider for each cut
-	slider_X = Slider(axlayer_X, "Sagittal Cut", 0, image_data.shape[0] - 1, valinit=half_X, valstep=1)
-	slider_Y = Slider(axlayer_Y, "Coronal Cut", 0, image_data.shape[1] - 1, valinit=half_Y, valstep=1)
-	slider_Z = Slider(axlayer_Z, "Horizontal Cut", 0, image_data.shape[2] - 1, valinit=half_Z, valstep=1)
+	slider_X = Slider(axlayer_X, "Sagittal Cut", 0, brain_data.shape[0] - 1, valinit=half_X, valstep=1)
+	slider_Y = Slider(axlayer_Y, "Coronal Cut", 0, brain_data.shape[1] - 1, valinit=half_Y, valstep=1)
+	slider_Z = Slider(axlayer_Z, "Horizontal Cut", 0, brain_data.shape[2] - 1, valinit=half_Z, valstep=1)
 
 	# Set title for each ax
-	ax[0].set_title("{}/{}".format(half_X, image_data.shape[0] - 1))
-	ax[1].set_title("{}/{}".format(half_Y, image_data.shape[1] - 1))
-	ax[2].set_title("{}/{}".format(half_Z, image_data.shape[2] - 1))
+	ax[0].set_title("{}/{}".format(half_X, brain_data.shape[0] - 1))
+	ax[1].set_title("{}/{}".format(half_Y, brain_data.shape[1] - 1))
+	ax[2].set_title("{}/{}".format(half_Z, brain_data.shape[2] - 1))
 
 	# Update function for each respective slider and it's plot
 	def update_X(val):
 		layer = slider_X.val
-		ax[0].imshow(image_data[layer,:,:], cmap=image_color_map)
-		if seg_filename:
+		ax[0].imshow(brain_data[layer,:,:], cmap=brain_color_map)
+		if seg_data is not None:
 			ax[0].imshow(seg_data[layer,:,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[layer,:,:]>0))
-		ax[0].set_title("{}/{}".format(layer, image_data.shape[0] - 1))
+		ax[0].set_title("{}/{}".format(layer, brain_data.shape[0] - 1))
 		fig.canvas.draw_idle()
 
 	def update_Y(val):
 		layer = slider_Y.val
-		ax[1].imshow(image_data[:,layer,:], cmap=image_color_map)
-		if seg_filename:
+		ax[1].imshow(brain_data[:,layer,:], cmap=brain_color_map)
+		if seg_data is not None:
 			ax[1].imshow(seg_data[:,layer,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,layer,:]>0))
-		ax[1].set_title("{}/{}".format(layer, image_data.shape[1] - 1))
+		ax[1].set_title("{}/{}".format(layer, brain_data.shape[1] - 1))
 		fig.canvas.draw_idle()
 
 	def update_Z(val):
 		layer = slider_Z.val
-		ax[2].imshow(image_data[:,:,layer], cmap=image_color_map)
-		if seg_filename:
+		ax[2].imshow(brain_data[:,:,layer], cmap=brain_color_map)
+		if seg_data is not None:
 			ax[2].imshow(seg_data[:,:,layer], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,:,layer]>0))
-		ax[2].set_title("{}/{}".format(layer, image_data.shape[2] - 1))
+		ax[2].set_title("{}/{}".format(layer, brain_data.shape[2] - 1))
 		fig.canvas.draw_idle()
 
 
@@ -175,13 +157,13 @@ def display3DCuts(image_filename=None, seg_filename=None, image_color_map='bone'
 	
 
 	# First display
-	ax[0].imshow(image_data[half_X,:,:], cmap=image_color_map)
-	ax[1].imshow(image_data[:,half_Y,:], cmap=image_color_map)
-	ax[2].imshow(image_data[:,:,half_Z], cmap=image_color_map)
-	if seg_filename:
-		ax[0].imshow(seg_data[half_X,:,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[half_X,:,:]>0))
-		ax[1].imshow(seg_data[:,half_Y,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,half_Y,:]>0))
-		ax[2].imshow(seg_data[:,:,half_Z], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,:,half_Z]>0))
+	ax[0].imshow(brain_data[half_X,:,:], cmap=brain_color_map)
+	ax[1].imshow(brain_data[:,half_Y,:], cmap=brain_color_map)
+	ax[2].imshow(brain_data[:,:,half_Z], cmap=brain_color_map)
+	if seg_data is not None:
+		ax[0].imshow(seg_data[half_X,:,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[half_X,:,:]>0), vmin = 0)
+		ax[1].imshow(seg_data[:,half_Y,:], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,half_Y,:]>0), vmin = 0)
+		ax[2].imshow(seg_data[:,:,half_Z], cmap=seg_color_map, alpha=seg_alpha*(seg_data[:,:,half_Z]>0), vmin = 0)
 
 
 	# Turn off all of the axis
@@ -191,6 +173,3 @@ def display3DCuts(image_filename=None, seg_filename=None, image_color_map='bone'
 
 	# Show plot
 	plt.show()
-
-
-
